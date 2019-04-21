b609179826d9b7c7fc6557e7dbe709c55f723aba
liyuyan <4563599@qq.com>
2019-04-21 23:01:29 +0800

    异步请求登陆

--- crowding-manager/crowding-manager-impl/src/main/java/cn/lyy/crowdfunding/manager/controller/DispacherController.java
+++ crowding-manager/crowding-manager-impl/src/main/java/cn/lyy/crowdfunding/manager/controller/DispacherController.java
@@ -2,10 +2,12 @@ package cn.lyy.crowdfunding.manager.controller;
 
 import cn.lyy.crowdfunding.bean.User;
 import cn.lyy.crowdfunding.manager.service.UserService;
+import cn.lyy.utils.AjaxResult;
 import cn.lyy.utils.Const;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
@@ -33,14 +35,38 @@ public class DispacherController {
     }
 
 
+    //异步请求
+    @ResponseBody
     @RequestMapping("/doLogin")
-    public String doLogin(String loginacct, String userpswd, String type, HttpSession httpSession) {
-        Map<String, Object> paramMap = new HashMap<String, Object>();
-        paramMap.put("loginacct", loginacct);
-        paramMap.put("userpswd", userpswd);
-        paramMap.put("type", type);
-        User user = userService.login(paramMap);
-        httpSession.setAttribute(Const.LOGIN_USER, user);
-        return "redirect:/main.htm";
+    public Object doLogin(String loginacct, String userpswd, String type, HttpSession httpSession) {
+
+        AjaxResult result = new AjaxResult();
+
+        try {
+            Map<String, Object> paramMap = new HashMap<String, Object>();
+            paramMap.put("loginacct", loginacct);
+            paramMap.put("userpswd", userpswd);
+            paramMap.put("type", type);
+            User user = userService.login(paramMap);
+            httpSession.setAttribute(Const.LOGIN_USER, user);
+            result.setSuccess(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            result.setMessage("登录失败！");
+            result.setSuccess(false);
+        }
+        return result;
     }
+
+    //同步请求
+//    @RequestMapping("/doLogin")
+//    public String doLogin(String loginacct, String userpswd, String type, HttpSession httpSession) {
+//        Map<String, Object> paramMap = new HashMap<String, Object>();
+//        paramMap.put("loginacct", loginacct);
+//        paramMap.put("userpswd", userpswd);
+//        paramMap.put("type", type);
+//        User user = userService.login(paramMap);
+//        httpSession.setAttribute(Const.LOGIN_USER, user);
+//        return "redirect:/main.htm";
+//    }
 }
